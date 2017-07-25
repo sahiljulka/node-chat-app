981a51786d6b71bb321b0daa48e77b9c1a16a542
Sahil Julka <sahil.julka@nagarro.com>
2017-07-25 18:32:41 +0530

    add new message and createmessage events

--- public/js/index.js
+++ public/js/index.js
@@ -1,8 +1,30 @@
-	var socket=io();
-		socket.on('connect',()=>{
-			console.log("Connected to server");
-		})
+var socket=io();
 
-		socket.on('disconnect',()=>{
-			console.log("disconnect from server");
-		})
+socket.on('connect',function(){
+	console.log("Connected to server");
+/*	socket.emit('createEmail',{
+		"from":"sahiljulka44@gmail.com",
+		"body":"standupComedy",
+		"subject":"imp" 
+	});
+*/
+	socket.emit('newMessage',{
+		"from":"sahiljulka",
+		"body":"hello from client"
+	});
+})
+
+socket.on('disconnect',function(){
+	console.log("disconnect from server");
+})
+
+/*socket.on('newEmail',function(email){
+	console.log("New Email");
+	console.log(email); 
+})
+*/
+
+socket.on('chatMessage',function(message){debugger;
+	console.log("New Message");
+	console.log(message);
+})
